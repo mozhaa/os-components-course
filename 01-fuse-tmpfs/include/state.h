68e2d2934f2b1d4585994a7a071c0e6dc6d5ddd3
mozhaa <mozhay2005@gmail.com>
2026-02-18 11:49:36 +0300

    feat: define tmpfs_inode, dirent and state

--- 01-fuse-tmpfs/include/state.h
+++ 01-fuse-tmpfs/include/state.h
@@ -1,7 +1,36 @@
 #ifndef TMPFS_STATE_H_
 #define TMPFS_STATE_H_
 
-struct tmpfs_state {};
+#include <sys/types.h>
+
+struct tmpfs_inode {
+    int mode;
+    uid_t uid;
+    gid_t gid;
+    struct timespec atime, mtime, ctime;
+
+    union {
+        struct {
+            size_t size;
+            char *data;
+        } file;
+        struct {
+            struct dirent *entries;
+            int entries_size;
+            int entries_capacity;
+        } dir;
+    } content;
+};
+
+struct tmpfs_dirent {
+    char *name;
+    struct tmpfs_inode *inode;
+};
+
+struct tmpfs_state {
+    struct tmpfs_inode *root;
+};
+
 #define TMPFS_DATA ((struct tmpfs_state *)fuse_get_context()->private_data)
 
 #endif // TMPFS_STATE_H_
