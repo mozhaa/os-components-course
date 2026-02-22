ce09d2b3937bcb8e14e1b7de45a63483f7b5d85f
mozhaa <mozhay2005@gmail.com>
2026-02-22 14:25:23 +0300

    fix: add null-terminator after strncpy, don't omit curly braces for single-line blocks

--- 01-fuse-tmpfs/src/state.c
+++ 01-fuse-tmpfs/src/state.c
@@ -9,18 +9,22 @@
 int path_lookup(struct tmpfs_inode *root, const char *path, struct tmpfs_inode **parent, char *name,
                 struct tmpfs_inode **child) {
     if (strcmp(path, "/") == 0) {
-        if (parent)
+        if (parent) {
             *parent = NULL;
-        if (name)
+        }
+        if (name) {
             name[0] = '\0';
-        if (child)
+        }
+        if (child) {
             *child = root;
+        }
         return 0;
     }
 
     char *path_copy = strdup(path);
-    if (!path_copy)
+    if (!path_copy) {
         return -ENOMEM;
+    }
 
     struct tmpfs_inode *current = root;
     struct tmpfs_inode *prev = NULL;
@@ -49,12 +53,16 @@ int path_lookup(struct tmpfs_inode *root, const char *path, struct tmpfs_inode *
                 free(path_copy);
                 return -ENOENT;
             } else {
-                if (parent)
+                if (parent) {
                     *parent = current;
-                if (name)
-                    strncpy(name, token, TMPFS_NAME_MAX_LENGTH + 1);
-                if (child)
+                }
+                if (name) {
+                    strncpy(name, token, TMPFS_NAME_MAX_LENGTH);
+                    name[TMPFS_NAME_MAX_LENGTH + 1] = 0;
+                }
+                if (child) {
                     *child = NULL;
+                }
                 free(path_copy);
                 return 0;
             }
@@ -65,19 +73,24 @@ int path_lookup(struct tmpfs_inode *root, const char *path, struct tmpfs_inode *
         }
     }
 
-    if (parent)
+    if (parent) {
         *parent = prev;
-    if (name)
-        strncpy(name, last_token, TMPFS_NAME_MAX_LENGTH + 1);
-    if (child)
+    }
+    if (name) {
+        strncpy(name, last_token, TMPFS_NAME_MAX_LENGTH);
+        name[TMPFS_NAME_MAX_LENGTH + 1] = 0;
+    }
+    if (child) {
         *child = current;
+    }
     free(path_copy);
     return 0;
 }
 
 struct tmpfs_inode *find_inode(struct tmpfs_inode *root, const char *path) {
     struct tmpfs_inode *child;
-    if (path_lookup(root, path, NULL, NULL, &child) != 0)
+    if (path_lookup(root, path, NULL, NULL, &child) != 0) {
         return NULL;
+    }
     return child;
 }
