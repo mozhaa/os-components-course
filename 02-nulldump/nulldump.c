30fa2754a3886e26f854656913e86691ee23486a
mozhaa <mozhay2005@gmail.com>
2026-02-26 11:03:08 +0300

    feat: write hexdump of written data into dmesg

--- 02-nulldump/nulldump.c
+++ 02-nulldump/nulldump.c
@@ -1,3 +1,4 @@
+#include <linux/ctype.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -8,11 +9,50 @@ MODULE_DESCRIPTION("Character device driver which works like /dev/null but also 
 MODULE_VERSION("0.1");
 
 #define DEVICE_NAME "nulldump"
+#define LINE_WIDTH 16
 static int major;
 
 static ssize_t nulldump_read(struct file *file, char __user *buf, size_t size, loff_t *ppos) { return 0; }
 
-static ssize_t nulldump_write(struct file *file, const char __user *data, size_t size, loff_t *ppos) { return size; }
+static ssize_t nulldump_write(struct file *file, const char __user *data, size_t size, loff_t *ppos) {
+    size_t offset = 0;
+    unsigned char linebuf[LINE_WIDTH];
+    char outbuf[LINE_WIDTH * 4 + 20];
+
+    while (offset < size) {
+        size_t chunk = size - offset;
+        size_t i, bytes_this_line = (chunk < LINE_WIDTH) ? chunk : LINE_WIDTH;
+        unsigned long uncopied;
+        char *p = outbuf;
+
+        uncopied = copy_from_user(linebuf, data + offset, bytes_this_line);
+        if (uncopied) {
+            return -EFAULT;
+        }
+
+        for (i = 0; i < bytes_this_line; i++) {
+            p += sprintf(p, "%02x ", linebuf[i]);
+        }
+
+        for (i = bytes_this_line; i < LINE_WIDTH; i++) {
+            p += sprintf(p, "   ");
+        }
+
+        p += sprintf(p, " |");
+
+        for (i = 0; i < bytes_this_line; i++) {
+            *p++ = isprint(linebuf[i]) ? linebuf[i] : '.';
+        }
+
+        p += sprintf(p, "|");
+        *p = 0;
+
+        pr_info("nulldump: %s\n", outbuf);
+        offset += bytes_this_line;
+    }
+
+    return size;
+}
 
 static struct file_operations nulldump_fops = {
     .owner = THIS_MODULE,
