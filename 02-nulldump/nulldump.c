56883cc7124e60f3b611d59ddf41b0beabb4b80e
mozhaa <mozhay2005@gmail.com>
2026-03-03 09:36:51 +0300

    feat: create /dev/nulldump on module init (and also set permissions to 0666)

--- 02-nulldump/nulldump.c
+++ 02-nulldump/nulldump.c
@@ -1,4 +1,5 @@
 #include <linux/ctype.h>
+#include <linux/device.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -10,8 +11,12 @@ MODULE_DESCRIPTION("Character device driver which works like /dev/null but also 
 MODULE_VERSION("0.1");
 
 #define DEVICE_NAME "nulldump"
+#define CLASS_NAME "nulldump"
 #define LINE_WIDTH 16
+
 static int major;
+static struct class *nulldump_class = NULL;
+static struct device *nulldump_device = NULL;
 
 static ssize_t nulldump_read(struct file *file, char __user *buf, size_t size, loff_t *ppos) {
     pr_info("nulldump: read of %lu bytes from pid=%d, comm=%s\n", size, current->pid, current->comm);
@@ -60,6 +65,13 @@ static ssize_t nulldump_write(struct file *file, const char __user *data, size_t
     return size;
 }
 
+static char *nulldump_devnode(const struct device *dev, umode_t *mode) {
+    if (mode) {
+        *mode = 0666;
+    }
+    return NULL;
+}
+
 static struct file_operations nulldump_fops = {
     .owner = THIS_MODULE,
     .read = nulldump_read,
@@ -67,16 +79,45 @@ static struct file_operations nulldump_fops = {
 };
 
 static int __init nulldump_start(void) {
+    int ret;
+
     major = register_chrdev(0, DEVICE_NAME, &nulldump_fops);
     if (major < 0) {
-        pr_err("nulldump: failed to register device: %d\n", major);
-        return major;
+        ret = major;
+        pr_err("nulldump: failed to register device: %d\n", ret);
+        goto fail_operations;
     }
-    pr_info("nulldump: registered device with major number %d\n", major);
+
+    nulldump_class = class_create(CLASS_NAME);
+    if (IS_ERR(nulldump_class)) {
+        ret = PTR_ERR(nulldump_class);
+        pr_err("nulldump: failed to create class: %d\n", ret);
+        goto fail_class;
+    }
+
+    nulldump_class->devnode = nulldump_devnode;
+
+    nulldump_device = device_create(nulldump_class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(nulldump_device)) {
+        ret = PTR_ERR(nulldump_device);
+        pr_err("nulldump: failed to create device: %d\n", ret);
+        goto fail_device;
+    }
+
+    pr_info("nulldump: created device at /dev/%s\n", DEVICE_NAME);
     return 0;
+
+fail_device:
+    class_destroy(nulldump_class);
+fail_class:
+    unregister_chrdev(major, DEVICE_NAME);
+fail_operations:
+    return ret;
 }
 
 static void __exit nulldump_end(void) {
+    device_destroy(nulldump_class, MKDEV(major, 0));
+    class_destroy(nulldump_class);
     unregister_chrdev(major, DEVICE_NAME);
     pr_info("nulldump: unregistered device\n");
 }
